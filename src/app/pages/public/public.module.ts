270fa5b7e4b18adbb3cf8e0e5b10bbca4703b2b7
Joseph Vallejo <joseph.2210@hotmail.es>
2025-06-16 05:25:20 -0500

    feat: home

--- src/app/pages/public/public.module.ts
+++ src/app/pages/public/public.module.ts
@@ -1,19 +1,25 @@
 import { PublicLayoutComponent } from '../../layouts/public-layout/public-layout.component';
 import { PublicRoutingModule } from './public-routing.module';
 import { SharedModule } from '../../shared/shared.module';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
-    PublicLayoutComponent
+    PublicLayoutComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     PublicRoutingModule,
     CommonModule,
     RouterModule,
-    SharedModule
+    SharedModule,
+    ButtonModule
   ],
   exports: [
     PublicLayoutComponent
